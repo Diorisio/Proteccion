2faff87d6c8698affdf72bdee4a6bab63c0daa41
Diorisio <r578@hotmail.com>
2022-03-16 12:16:38 -0500

    Serie

---
+++ public/javascripts/index.js
@@ -0,0 +1,29 @@
+    window.onload=function(){
+    pboton=document.querySelector('.boton')
+    phoras=document.querySelector('.horas')
+    pminutos=document.querySelector('.minutos')
+    psegundos=document.querySelector('.segundos')
+    pserie=document.querySelector('.serie')
+    const serie=[]
+    
+
+    const fecha= new Date();
+    const horas = fecha.getHours();
+    const minutos = fecha.getMinutes();
+    const segundos = fecha.getSeconds();
+
+    phoras.innerText = horas;
+    pminutos.innerText += minutos;
+    psegundos.innerText += segundos;
+    
+    const sminutos=minutos.toString();
+    serie.push(sminutos[0])
+    serie.push(sminutos[1])?serie.push(sminutos[1]):serie.push(0)
+    
+    for(let i=0;i<segundos;i++){
+        serie.push(parseInt(serie[i])+parseInt(serie[i+1]))
+        
+
+    }
+    pserie.innerText = serie;
+}
